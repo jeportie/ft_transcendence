3669db417aa03a39d38cc1644c7abd12481d07c7
Jerome Portier <jeromep.dev@gmail.com>
2025-08-05 19:07:57 +0200

    push pause

--- pong/main.js
+++ pong/main.js
@@ -6,41 +6,49 @@
 //   By: jeportie <jeportie@42.fr>                  +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2025/08/04 17:53:15 by jeportie          #+#    #+#             //
-//   Updated: 2025/08/04 18:36:27 by jeportie         ###   ########.fr       //
+//   Updated: 2025/08/05 19:04:36 by jeportie         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
 import Ball from "./Ball.class.js";
+import Paddle from "./Paddle.class.js";
 import Position from "./Position.class.js";
 import Vector from "./Vector2D.class.js";
+import Draw2D from "./Draw2D.class.js";
 
 const canvas = document.querySelector("#gameCanvas");
 const ctx = canvas.getContext('2d');
+const draw = new Draw2D(ctx);
 
-// Create a random‐direction velocity vector of fixed speed
-const speed = 5;
+const speed = 4.123;
 const angleRad = 1.5708;
-const velocity = new Vector(1, 0)      // unit vector along +x
-	.rotate(angleRad)                     // spin it to a random angle
-	.scale(speed);                        // bump it out to 'speed' length
-
-// Centered starting position
+const velocity = new Vector(1, 0).rotate(angleRad).scale(speed);
 const startPosition = new Position(canvas.width / 2, canvas.height / 2);
-
-// Put it all together
-const ball = new Ball(startPosition, velocity, 10, canvas, ctx);
+const paddlePos = new Position(5, canvas.height / 2);
+const ball = new Ball(startPosition, velocity, 10, canvas, draw);
+const paddle = new Paddle(paddlePos, canvas, draw);
 
 // Main loop
 function gameLoop() {
-	// Clear
-	ctx.fillStyle = '#000000';
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // Clear
+    draw.rect(0, 0, canvas.width, canvas.height, { fillStyle: "#000000" });
 
-	// Update & draw
-	ball.update();
-	ball.draw();
+    // draw center dashed line
+    draw.line(
+        canvas.width / 2, 0,
+        canvas.width / 2, canvas.height,
+        { strokeStyle: "#555555", lineWidth: 4, dash: [10, 10] }
+    );
 
-	requestAnimationFrame(gameLoop);
+    paddle.update();
+    paddle.draw();
+
+    // Update & draw
+    ball.update();
+    ball.draw();
+
+    requestAnimationFrame(gameLoop);
 }
+
 
 gameLoop();
