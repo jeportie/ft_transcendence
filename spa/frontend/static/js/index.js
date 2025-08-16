d53409b7ce53b6b3c6c8f0c1bd7e25b4b232e1a8
jeromeDev94 <jeromep.dev@gmail.com>
2025-08-16 19:29:40 +0200

    push pause

--- spa/frontend/static/js/index.js
+++ spa/frontend/static/js/index.js
@@ -15,17 +15,30 @@ import Posts from "./views/Posts.js";
 import Settings from "./views/Settings.js";
 
 function pathToRegex(path) {
-    return (new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$"));
+    return (new RegExp("^" + path
+        .replace(/\//g, "\\/")
+        .replace(/:\w+/g, "([^\\/]+)") + "$"));
 }
 
 function getParams(match) {
     const values = match.result.slice(1);
-    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+    const keys = Array.from(match.route.path
+        .matchAll(/:(\w+)/g))
+        .map(result => result[1]);
 
     return (Object.fromEntries(keys.map((key, i) => {
-        return ([key, values[i]]);
+        return ([key, decodeURIComponent(values[i])]);
     })));
 }
+
+function normalize(path) {
+    if (path !== "/")
+        return (path.replace(/\/+$/, ""));
+    else
+        return (path);
+}
+
+
 
 function navigateTo(url) {
     history.pushState(null, null, url);
@@ -33,6 +46,9 @@ function navigateTo(url) {
 }
 
 async function router() {
+
+    const pathname = normalize(location.pathname);
+
     const routes = [
         { path: "/", view: Dashboard },
         { path: "/posts", view: Posts },
@@ -43,7 +59,7 @@ async function router() {
     const potentialMatches = routes.map(route => {
         return ({
             route,
-            result: location.pathname.match(pathToRegex(route.path)),
+            result: pathname.match(pathToRegex(route.path)),
         });
     });
 
