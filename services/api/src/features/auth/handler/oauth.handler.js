566c161b81127e45687340046599f55bcc13d34c
jeromep <jeromep.dev@gmail.com>
2025-09-25 19:36:34 +0200

    push pause

---
+++ services/api/src/features/auth/handler/oauth.handler.js
@@ -0,0 +1,22 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   oauth.handler.js                                   :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: jeportie <jeportie@42.fr>                  +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2025/09/25 19:26:48 by jeportie          #+#    #+#             //
+//   Updated: 2025/09/25 19:36:00 by jeportie         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+import * as controller from "../controller/local.controller.js";
+import { startSchema } from "../schema/startSchema.js";
+import { callbackSchema } from "../schema/callbackSchema.js";
+
+export async function localRoutes(fastify, options) {
+
+    fastify.get("/:provider/start", { schema: startSchema }, controller.startOAuth);
+    fastify.get("/:provider/callback", { schema: callbackSchema }, controller.handleOAuth);
+
+}
