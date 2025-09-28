2cfd77b0189b372f03d9ff6ea7c0f0c3c7eb8e18
jeromep <jeromep.dev@gmail.com>
2025-09-28 15:02:11 +0200

    push pause

--- services/api/src/features/user/handler/admin.handler.js
+++ services/api/src/features/user/handler/admin.handler.js
@@ -6,7 +6,7 @@
 //   By: jeportie <jeportie@42.fr>                  +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2025/09/25 18:22:17 by jeportie          #+#    #+#             //
-//   Updated: 2025/09/25 18:43:28 by jeportie         ###   ########.fr       //
+//   Updated: 2025/09/28 14:42:40 by jeportie         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -14,11 +14,7 @@ import * as controller from "../controller/admin.controller.js";
 import { usersSchema } from "../schema/usersSchema.js";
 
 export async function adminRoutes(fastify, options) {
+    const authGuard = { preHandler: [fastify.authorize("admin")] };
 
-    fastify.get(
-        "/users",
-        { schema: usersSchema, preHandler: [fastify.authorize("admin")] },
-        controller.getUsers
-    );
-
+    fastify.get("/users", { ...authGuard, schema: usersSchema }, controller.getUsers);
 }
--- services/api/src/features/user/handler/user.handler.js
+++ services/api/src/features/user/handler/user.handler.js
@@ -6,7 +6,7 @@
 //   By: jeportie <jeportie@42.fr>                  +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2025/09/25 15:04:53 by jeportie          #+#    #+#             //
-//   Updated: 2025/09/25 15:13:34 by jeportie         ###   ########.fr       //
+//   Updated: 2025/09/28 14:46:27 by jeportie         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -14,11 +14,6 @@ import * as controller from "../controller/user.controller.js";
 import { meSchema } from "../schema/meSchema.js";
 
 export async function userRoutes(fastify, options) {
-
-    fastify.get(
-        "/me",
-        { schema: meSchema, preHandler: [fastify.authenticate] },
-        controller.getMe
-    );
-
+    const authGuard = { preHandler: [fastify.authenticate] };
+    fastify.get("/me", { ...authGuard, schema: meSchema }, controller.getMe);
 }
